f2ee37513857cb64f57f05fbaf2c96ef5f1030b3
Wahab-dev09 <wahabzsec@gmail.com>
2024-09-02 23:12:54 -0600

    Details Section Updated

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -6,7 +6,7 @@ const Detail = () => {
     const svg1 = (<svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="rgb(255, 164, 164)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z" /><circle cx="12" cy="12" r="10" /></svg>);
     const svg2 = (<svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="rgb(78, 196, 160)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 6h4" /><path d="M2 10h4" /><path d="M2 14h4" /><path d="M2 18h4" /><rect width="16" height="20" x="4" y="2" rx="2" /><path d="M9.5 8h5" /><path d="M9.5 12H16" /><path d="M9.5 16H14" /></svg>);
     const svg3 = (<svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="rgb(87, 66, 167)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><rect width="7" height="7" x="14" y="3" rx="1" /><path d="M10 21V8a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H3" /></svg>);
-    const arr = [
+    const arr = [ 
         {head: "Exploration",para: "If something happens to your home - or any attached structures on your property - we will cover the repairs on the rebuild.",detsvg: svg1,},
         {head: "Work assignments",para: "It's not just stuff it's your life. And if there's ever a theft, fire or any other unfortunate event we'll help with the repair or replcement.",detsvg: svg2,},
         {head: "Entended stays",para: "Our coverage protects you from bodily injury or property damage or accidents to others (or their stuff) - at home or anywher else.",detsvg: svg3,},
