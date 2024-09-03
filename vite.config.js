9f0c69c580155f71893cad3233559ce05264ff5b
Wahab-dev09 <wahabzsec@gmail.com>
2024-09-03 04:46:37 -0600

    Final Setuo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/All-state/',
+  base: '/Real-state/',
   build: {
     minify: 'terser',
     terserOptions: {
